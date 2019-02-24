920814619697c91c93b8d95662247cc8d82edaed
felix <2319979647@qq.com>
2019-02-24 21:33:53 +0800

    fix : add th stl

---
+++ STL/STL_workspace/STL_workspace/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STL_workspace
+//
+//  Created by Aka on 2019/2/24.
+//  Copyright © 2019 Aka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
